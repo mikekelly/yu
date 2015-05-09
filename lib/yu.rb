a01257d2737d5a7a06c0f5588dcb5be59af6cf6b
Mike Kelly <mikekelly321@gmail.com>
2015-05-09 11:34:54 +0100

    spike executable

--- lib/yu.rb
+++ lib/yu.rb
@@ -1,5 +1,29 @@
-require "yu/version"
+require 'yu/version'
+require 'commander'
 
 module Yu
-  # Your code goes here...
+  class CLI
+    include Commander::Methods
+
+    def self.call(*args)
+      new(*args).call
+    end
+
+
+    def call
+      program :name, 'yu'
+      program :version, VERSION
+      program :description, 'TODO'
+
+      command :test do |c|
+        c.syntax = 'yu test'
+        c.description = 'Just a test'
+        c.action do |args, options|
+          say 'Hello world'
+        end
+      end
+
+      run!
+    end
+  end
 end
